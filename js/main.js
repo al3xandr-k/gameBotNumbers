0ba8b285587f6853897571d10795a3500a19dd00
Alexander Kupsha <red2@mail.ru>
2021-02-09 12:52:56 +0200

    Finished task

--- js/main.js
+++ js/main.js
@@ -2,29 +2,37 @@
 
 
 
-const gameBot = () => {
-  let num = 10;
+const gameBot = (x, y) => {
+  let num = Math.floor(Math.random() * (x + y) + x);
+  console.log('num: ', num);
 
   return function () {
     let str = prompt('Угадайте число от 1 до 100.');
-    console.log('str: ', str);
 
     if (str === null) {
       console.log('Игра окончена.');
+      return;
     } else if (str > num) {
       console.log('Загаданное число меньше.');
+      str = prompt('Угадайте число от 1 до 100.');
+      return;
     } else if (str !== str.trim()) {
       console.log('Введи число! А не пустую строку.');
+      return;
     } else if (str < num) {
       console.log('Загаданное число больше');
+      str = prompt('Угадайте число от 1 до 100.');
+      return;
     } else if (+str === num) {
       console.log('Поздравляю, Вы угадали!!!');
+      return;
     } else if (typeof str !== 'number') {
       console.log('Введи число! А не строку.');
-
+      return;
     }
   }
 }
 
-const game = gameBot();
+const game = gameBot(1, 100);
 game();
+
