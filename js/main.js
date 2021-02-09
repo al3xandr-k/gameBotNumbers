97f84199a7617b3d3695031bb96878274cf60c55
Alexander Kupsha <red2@mail.ru>
2021-02-09 19:52:22 +0200

    added changes

--- js/main.js
+++ js/main.js
@@ -5,38 +5,30 @@ const gameBot = (x, y) => {
   let num = Math.floor(Math.random() * (x + y) + x);
   let i = 10;
 
-  function chanceLeft() {
-    i--;
-    console.log('i: ', i);
-    if (i >= 0) {
-      return i;
-    }
-  };
-
-  return function () {
+  const askFunc = () => {
 
     let str = prompt('Угадайте число от 1 до 100.');
 
-    if (chanceLeft() === 0) {
+    if (i === 0) {
       confirm('Попытки закончились, хотите сыграть еще?');
     } else if (str === null) {
       console.log('Game Over');
       return;
     } else if (str > num) {
-      console.log(`Загаданное число меньше, осталось попыток ${chanceLeft()}`);
-      str;
+      console.log(`Загаданное число меньше, осталось попыток`);
+
     } else if (str !== str.trim()) {
       console.log('Введи число! А не пустую строку.');
     } else if (str < num) {
-      console.log(`Загаданное число больше, осталось попыток ${chanceLeft()}`);
-      str;
+      console.log(`Загаданное число больше, осталось попыток`);
+
     } else if (+str === num) {
       confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
     } else if (typeof str !== 'number') {
       console.log('Введи число! А не строку.');
     }
   }
+  askFunc();
 }
 
-const game = gameBot(1, 100);
-game();
+gameBot(1, 100);
