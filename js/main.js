23e274b188c506f47dc36d62fc0d90eee6d99e5c
Alexander Kupsha <red2@mail.ru>
2021-02-11 18:53:48 +0200

    не срабатывает проверка пустой строки

--- js/main.js
+++ js/main.js
@@ -1,38 +1,45 @@
 'use strict'
 
 const gameBot = (x, y) => {
-
-  let num = Math.floor(Math.random() * (x + y) + x);
+  let randomNumbers = Math.floor(Math.random() * (x + y) + x);
   let i = 10;
+  console.log('randomNumbers: ', randomNumbers); //Данная строчка указывает число, для проверки условие когда randomNumbers === playerNumber;
 
-  const askFunc = () => {
+  const gameConditions = () => {
     i--;
-    let str = prompt('Угадайте число от 1 до 100.');
-    console.log(typeof +str);
+    let playerNumber = prompt('Загадывание случайного числа от 1 до 100.');
 
-    if (i === 0) {
-      confirm('Попытки закончились, хотите сыграть еще?');
-      gameBot();
-    } else if (str === null) {
+    if (typeof playerNumber === 'object') {
       alert('Game Over');
-      return;
-    } else if (str > num) {
+    } else if (i === 0) {
+      let chanceEnd = confirm('Попытки закончились, хотите сыграть еще?');
+      if (chanceEnd === true) {
+        gameBot(1, 10);
+      } else {
+        alert('Как хочешь, дружище! Приходи ещё.');
+      }
+    } else if (playerNumber > randomNumbers) {
       alert(`Загаданное число меньше, осталось попыток ${i}`);
-      gameBot(1, 100);
-    } else if (str !== str.trim()) {
-      alert('Введи число! А не пустую строку.');
-      gameBot();
-    } else if (str < num) {
+      gameConditions();
+    } else if (playerNumber < randomNumbers) {
       alert(`Загаданное число больше, осталось попыток ${i}`);
-      gameBot(1, 100);
-    } else if (typeof +str !== 'number') {
+      gameConditions();
+    } else if (Number(playerNumber) === randomNumbers) {
+      let finalQuestion = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
+      if (finalQuestion === true) {
+        gameBot(1, 10);
+      } else {
+        alert('Как хочешь, дружище!');
+      };
+    } else if (playerNumber !== playerNumber.trim() && playerNumber !== '') {
+      alert('Введи число! А не пустую строку.');
+      gameConditions();
+      //не работает!!!!!
+    } else if (typeof playerNumber !== 'number') {
       alert('Введи число! А не строку.');
-      gameBot();
-    } else if (+str === num) {
-      alert('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
+      gameConditions();
     }
-  } 
-  askFunc();
+  }
+  gameConditions();
 }
-
-gameBot(1, 100);
+gameBot(1, 10);
