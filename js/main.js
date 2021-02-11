12306a1a8c12f12a8ec4220300aaa05b34ce951f
Alexander Kupsha <red2@mail.ru>
2021-02-11 19:03:35 +0200

    Finished task gameBot_Hard

--- js/main.js
+++ js/main.js
@@ -3,7 +3,9 @@
 const gameBot = (x, y) => {
   let randomNumbers = Math.floor(Math.random() * (x + y) + x);
   let i = 10;
-  console.log('randomNumbers: ', randomNumbers); //Данная строчка указывает число, для проверки условие когда randomNumbers === playerNumber;
+  
+  console.log('randomNumbers: ', randomNumbers); 
+  //Данная строчка указывает число, для проверки условие когда randomNumbers === playerNumber;
 
   const gameConditions = () => {
     i--;
@@ -14,10 +16,13 @@ const gameBot = (x, y) => {
     } else if (i === 0) {
       let chanceEnd = confirm('Попытки закончились, хотите сыграть еще?');
       if (chanceEnd === true) {
-        gameBot(1, 10);
+        gameBot(1, 100);
       } else {
         alert('Как хочешь, дружище! Приходи ещё.');
       }
+    } else if (playerNumber !== playerNumber.trim() && playerNumber !== '') {
+      alert('Введи число! А не пустую строку.');
+      gameConditions();
     } else if (playerNumber > randomNumbers) {
       alert(`Загаданное число меньше, осталось попыток ${i}`);
       gameConditions();
@@ -27,14 +32,10 @@ const gameBot = (x, y) => {
     } else if (Number(playerNumber) === randomNumbers) {
       let finalQuestion = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
       if (finalQuestion === true) {
-        gameBot(1, 10);
+        gameBot(1, 100);
       } else {
         alert('Как хочешь, дружище!');
       };
-    } else if (playerNumber !== playerNumber.trim() && playerNumber !== '') {
-      alert('Введи число! А не пустую строку.');
-      gameConditions();
-      //не работает!!!!!
     } else if (typeof playerNumber !== 'number') {
       alert('Введи число! А не строку.');
       gameConditions();
@@ -42,4 +43,4 @@ const gameBot = (x, y) => {
   }
   gameConditions();
 }
-gameBot(1, 10);
+gameBot(1, 100);
